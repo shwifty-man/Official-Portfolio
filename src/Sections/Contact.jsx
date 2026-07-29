9b491d31343ae8338c146fa2cefb682c62fd66ed
Timothy <TimothyW.dev@gmail.com>
2026-07-29 19:40:42 -0400

    add messaging to contact form

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -1,12 +1,43 @@
-import GithubSection from "..//Content/Github-cons.jsx"
+import GithubSection from "../Content/Github-cons.jsx"
 import { FaUser, FaEnvelope, FaRegMessage, FaRegEnvelope } from "react-icons/fa6"
 import Footer from '../Sections/Footer.jsx'
 import "../Styles/Footer.css"
+import { useState } from "react"
 
 function Contact() {
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("Form submitted");
+    const [status, setStatus] = useState("")
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch(import.meta.env.VITE_BACKEND_CONTACT_FORM, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+
+        if (response.ok) {
+            setStatus("Message sent!")
+
+            setTimeout(() => {
+                setStatus("")
+            }, 2000)
+
+            setFormData({
+                name: "",
+                email: "",
+                message: "",
+            })
+        } else {
+            setStatus("Failed to send message")
+        }
     }
 
     return (
@@ -21,8 +52,11 @@ function Contact() {
                         <h2>Send a Message</h2>
                         <p>I'll get back to you within 24 hours.</p>
                     </div>
-                    <label className="label" for="input-name">{<FaUser size={15} color="#333" />}  YOUR NAME</label>
+                    <label className="label" htmlFor="input-name">{<FaUser size={15} color="#333" />}  YOUR NAME</label>
                     <input
+                        name="name"
+                        value={formData.name}
+                        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                         id="input-name"
                         type="text"
                         placeholder="John Doe"
@@ -30,24 +64,31 @@ function Contact() {
                         required
                     />
 
-                    <label className="label" for="input-email">{<FaEnvelope size={15} color="#333" />}  YOUR EMAIL</label>
+                    <label className="label" htmlFor="input-email">{<FaEnvelope size={15} color="#333" />}  YOUR EMAIL</label>
                     <input
+                        name="email"
+                        value={formData.email}
+                        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                         id="input-email"
                         type="email"
                         placeholder="JohnDoe@gmail.com"
                         className="contact-input"
                         required
                     />
-                    <label className="label" for="textarea-id">{<FaRegMessage size={15} color="#333" />}  YOUR MESSAGE</label>
+                    <label className="label" htmlFor="textarea-id">{<FaRegMessage size={15} color="#333" />}  YOUR MESSAGE</label>
                     <textarea
+                        name="textarea"
+                        value={formData.message}
+                        onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                         id="textarea-id"
                         placeholder="Tell me about your project..."
                         className="contact-textarea"
                         required
-                        placeholderColor="#333"
                     />
 
-                    <button type="submit" className="contact-button">Send Message {<FaRegEnvelope size={15} color="#ffffff" />}</button>
+                    <button type="submit" className="contact-button">
+                        {status ? (<p style={{ color: "#02831e", margin: "0px" }}>{status}</p>) : (<>Send Message {<FaRegEnvelope size={15} color="#ffffff" />} </>)}
+                    </button>
                 </form>
             </div>
             <Footer />
