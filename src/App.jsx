ebb5c27d9a5ddfb75ec27401d03af2227fde5c7c
Timothy <TimothyW.dev@gmail.com>
2026-07-29 18:56:47 -0400

    create links on the side of the screen for Github, LinkedIn, and to download resume

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import "./Styles/Skills.css"
 import "./Styles/Projects.css"
 import "./Styles/Contact.css"
 import "./Styles/Git.css"
+import "./Styles/Side-links.css"
 
 import { useRef } from "react";
 import Hero from './Sections/Hero.jsx'
 import Skills from './Sections/Skills.jsx'
 import Projects from './Sections/Projects.jsx'
 import Contact from './Sections/Contact.jsx'
+import SideLinks from './Content/Side-links.jsx'
 
 function App() {
   const targetElement = useRef(null);
   return (
     <>
+      <SideLinks />
       <div className="hero-outer-container">
         <div id="stars"></div>
         <div id="stars2"></div>
---
+++ src/Content/Side-links.jsx
@@ -0,0 +1,29 @@
+import { FaGithub, FaLinkedin, FaArrowCircleDown, FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
+import { handleProjectUrl } from "../helper.js"
+import { useState } from "react";
+
+export default function SideLinks() {
+    const [toggle, setToggle] = useState(false);
+    return (
+        <>
+            <div className={`side-links-container ${toggle ? "true" : "false"}`}>
+                <div
+                    onClick={() => setToggle(!toggle)}
+                    className={`toggle-arrow ${toggle ? "true" : "false"}`}>
+                    {toggle ? <FaArrowCircleLeft size={30} color="#fff" /> : <FaArrowCircleRight size={30} color="#fff" />}
+                </div>
+                <div onClick={handleProjectUrl("https://github.com/shwifty-man")} className="github-icon" title="GitHub">
+                    <FaGithub className="icon-div" size={40} color="#fff" />
+                </div>
+                <div onClick={handleProjectUrl("https://www.linkedin.com/in/timothy--c--williams/")} className="linkedIn-icon" title="LinkedIn">
+                    <FaLinkedin className="icon-div" size={40} color="#0A66C2" />
+                </div>
+                <div className="download-icon" title="Download Resume">
+                    <a id="download-a" href="/Timothy_Williams_Full-Stack_Engineer_Resume.pdf" download title="Download Resume">
+                        <FaArrowCircleDown size={40} color="#000" />
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
