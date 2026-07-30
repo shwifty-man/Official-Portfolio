1a2e01d1ef2fefa7a8f9a794b0ff54cac68023d5
Timothy <TimothyW.dev@gmail.com>
2026-07-30 15:42:16 -0400

    fix chat-button imports and use lazy correctly

--- src/Content/Chat-button.jsx
+++ src/Content/Chat-button.jsx
@@ -1,23 +1,28 @@
-import aiChatIcon from '../assets/chat-head-icon.png'
-const ChatView = lazy(() => import("./ChatView"));
-import { useState } from "react";
+import aiChatIcon from "../assets/chat-head-icon.png";
+import { useState, lazy, Suspense } from "react";
+
+const ChatView = lazy(() => import("./Chat-view.jsx"));
 
 function ChatButton() {
     const [toggle, setToggle] = useState(false);
 
-
     return (
         <>
-            <div className={`ai-chat-icon ${toggle}`} onClick={() => {
-                setToggle(!toggle)
-                console.log("toggle: ", toggle)
-            }} title="Resume Ai Chat">
-                <img src={aiChatIcon} alt="Ai chat button" />
+            <div
+                className="ai-chat-icon"
+                onClick={() => setToggle(!toggle)}
+                title="Resume AI Chat"
+            >
+                <img src={aiChatIcon} alt="AI chat button" />
             </div>
-            {toggle ? <ChatView onClose={() => setToggle(false)} /> : null}
+
+            {toggle && (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <ChatView onClose={() => setToggle(false)} />
+                </Suspense>
+            )}
         </>
     );
 }
-
 
 export default ChatButton;
